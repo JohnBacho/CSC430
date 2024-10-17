d19c06e0eb41784c88e70fcd8806656492b27481
JohnBacho <117942827+JohnBacho@users.noreply.github.com>
2024-10-17 14:19:08 -0400

    Add files via upload

---
+++ Boyer_Moore.py
@@ -0,0 +1,48 @@
+import time
+def search(txt, pat):
+    a = len(pat)
+    b = len(txt)
+    Char = charMismatch(pat, a)
+    r = 0
+    while(r <= b-a):
+        j = a-1
+        while j >= 0 and pat[j] == txt[r+j]:
+            j -= 1
+        if j < 0:
+            # print("Substring found at =", r)
+            r += (a-Char[ord(txt[r+a])] if r+a < b else 1)
+        else:
+            r += max(1, j-Char[ord(txt[r+j])])
+
+def charMismatch(string, size):
+    Characters = [-1]*256
+    for i in range(size):
+        Characters[ord(string[i])] = i
+    return Characters
+ 
+if __name__ == '__main__':
+
+    # Length of Shakespear 5458199
+    file1 = open("/Users/johnbacho/Desktop/Shakespear.txt", "r+")
+    txt = file1.read()
+    pat = input("Enter a string: ")
+    start = time.time()
+    search(txt, pat)
+    end = time.time()
+    print(end - start)
+
+    # Length of bee movie 86091
+    file2 = open("/Users/johnbacho/Desktop/Bee_movie.txt", "r+")
+    txt = file2.read()
+    start = time.time()
+    search(txt, pat)
+    end = time.time()
+    print(end - start)
+
+    # length of the Lottery 20049
+    file3 = open("/Users/johnbacho/Desktop/The Lottery.txt", "r+", encoding="utf-8")
+    txt = file3.read()
+    start = time.time()
+    search(txt, pat)
+    end = time.time()
+    print(end - start)
